887aef0cfa4890052fd5f37ca76d9c25555147a0
SaintLuan <luanfelipepp@hotmail.com>
2021-03-05 08:41:18 -0300

    Slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,14 +1,35 @@
-import styles from '../styles/components/Slider.module.css'
+import { useState } from 'react';
+import sliderData from '../../sliderData.json';
+import styles from '../styles/components/Slider.module.css';
+
+
 export function Slider(){
+    const [currentImg, setCurrentImg]=useState(0);
+
+    const goPrev = () => {
+        {currentImg===0 ?(
+            setCurrentImg(sliderData.length-1)
+        ):(
+            setCurrentImg(currentImg-1)
+        )}
+        console.log(currentImg);
+    }
+    const goNext = () => {
+        {currentImg===sliderData.length-1?(
+            setCurrentImg(0)
+        ):(
+            setCurrentImg(currentImg+1)
+        )}
+        console.log(currentImg);
+    }
+
     return(
         <div className={styles.sliderContainer}>
             <div className={styles.slider}>
-                <img src="img/capa-cursinho.jpg" alt="Capa Slider 1"/>
-
-                <span>.</span>
-            </div>
-            <div className={styles.slider}>
-                <img src="img/capa-Ensino-medio.jpg" alt="Capa Slider 2"/>
+                <img src={sliderData[currentImg].img} alt={sliderData[currentImg].alt}/>
+                <span onClick={goPrev} ><i className="uil uil-angle-left-b"></i></span>
+                <span onClick={goNext} ><i className="uil uil-angle-right-b"></i></span>
+                
             </div>
         </div>
     )
